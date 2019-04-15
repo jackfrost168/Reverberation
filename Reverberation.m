314106fd7b32277fe6a5812aca111e1566d8512e
Jack Frost <jack_frost118@163.com>
2019-04-15 11:03:19 +0800

    Update Reverberation.m

--- Reverberation.m
+++ Reverberation.m
@@ -1,125 +1,108 @@
-%%%  Schroeder»ìÏìÄ£ĞÍÓÉ8¸ö²¢ÁªµÄµÍÍ¨Êá×´ÂË²¨Æ÷£¬2¸ö´®ÁªµÄÈ«Í¨ÂË²¨Æ÷×é³É£¨¾ßÌåÄ£ĞÍ½á¹¹ºÍfilterº¯Êı²ÎÊıÇë¼ûpdfÎÄµµ£©
+%%%  Schroederæ··å“æ¨¡å‹ç”±8ä¸ªå¹¶è”çš„ä½é€šæ¢³çŠ¶æ»¤æ³¢å™¨ï¼Œ2ä¸ªä¸²è”çš„å…¨é€šæ»¤æ³¢å™¨ç»„æˆï¼ˆå…·ä½“æ¨¡å‹ç»“æ„å’Œfilterå‡½æ•°å‚æ•°è¯·è§pdfæ–‡æ¡£ï¼‰
 clc;close;clear;
-%%%²âÊÔÒôÆµ
-%filename = 'G:\Workfiles\Reverberation\Reverb_test\original.wav'
-%filename = 'G:\Workfiles\Reverberation\experiment\Algorithm Reverberation\music\Äã»¹ÒªÎÒÔõÑù Ñ¦Ö®Ç«.mp3'
-%filename = 'G:\Workfiles\Reverberation\experiment\Algorithm Reverberation\music\ÑİÔ± Ñ¦Ö®Ç«.mp3'
-%filename = 'G:\Workfiles\Reverberation\experiment\Algorithm Reverberation\ÅÄÊÖÉù\clap.wav'
-%filename = 'F:\JOYO\¸Ä½øSchroeder Reverberation£¨Ğ§¹û½ÏºÃ£©\²âÊÔÊı¾İ\clap.wav';
-filename = 'F:\JOYO\¸Ä½øSchroeder Reverberation£¨Ğ§¹û½ÏºÃ£©\²âÊÔÊı¾İ\music\See You Again.mp3';
-[X,FS]=audioread(filename); % ½« WAV ÎÄ¼ş×ª»»³É±äÁ¿
+%%%æµ‹è¯•éŸ³é¢‘
+%filename = 'G:\Workfiles\Reverberation\experiment\Algorithm Reverberation\æ‹æ‰‹å£°\clap.wav'
+%filename = 'F:\æ”¹è¿›Schroeder Reverberationï¼ˆæ•ˆæœè¾ƒå¥½ï¼‰\æµ‹è¯•æ•°æ®\clap.wav';
+filename = 'F:\æ”¹è¿›Schroeder Reverberationï¼ˆæ•ˆæœè¾ƒå¥½ï¼‰\æµ‹è¯•æ•°æ®\music\See You Again.mp3';
+[X,FS]=audioread(filename); % å°† WAV æ–‡ä»¶è½¬æ¢æˆå˜é‡
 n=8;
 %roomsize=input('please input a number between 0 and 1:');
-f=0.84*0.8;     %£¨0.74Îª¿Éµ÷½Ú¿Õ¼ä´óĞ¡£¬·¶Î§ÔÚ0-1Ö®¼ä£¬¿Éµ÷½Ú£©
+f=0.84*0.8;     %ï¼ˆ0.84ä¸ºæ§åˆ¶æ··å“ç©ºé—´å¤§å°å‚æ•°ï¼ŒèŒƒå›´åœ¨0-1ä¹‹é—´ï¼Œå¯è°ƒèŠ‚ï¼‰
 %sound(x,FS);
 %signal_out = filter(num,den,signal_in);
-% x = [-0.284149169921875,-0.145568847656250,-0.0744018554687500,0.260559082031250,0.737670898437500,...
-% 	0.948852539062500,0.992553710937500,0.956146240234375,0.679107666015625,0.287017822265625,-0.116699218750000,...
-% 	-0.524963378906250,-0.694641113281250,-0.619689941406250,-0.516754150390625,-0.367980957031250,-0.229705810546875,...
-% 	-0.137939453125000,0.128204345703125,0.465759277343750,0.500183105468750,0.226745605468750,-0.0614624023437500,...
-% 	-0.200866699218750,-0.363708496093750,-0.642333984375000,-0.722656250000000,-0.604919433593750,-0.602478027343750,...
-% 	-0.519592285156250,-0.251556396484375,-0.145385742187500,-0.102142333984375,0.0696105957031250,0.107513427734375,...
-% 	0.0235900878906250,0.00787353515625000,-0.0543823242187500,-0.134552001953125,-0.0986022949218750,-0.0508422851562500,...
-% 	-0.0410766601562500,0.0310058593750000,0.223114013671875,0.445617675781250,0.505432128906250,0.452301025390625,...
-% 	0.482635498046875,0.494934082031250,0.395690917968750,0.333587646484375,0.307800292968750,0.236663818359375,...
-% 	0.159271240234375,0.0686950683593750,-0.00662231445312500,-0.0108642578125000,-0.0312194824218750,-0.0548400878906250,...
-% 	0.00451660156250000,0.0303649902343750,0.0130004882812500,0.0502319335937500,-0.0303344726562500,-0.208923339843750,...
-% 	-0.167419433593750,-0.0798645019531250,-0.234039306640625,-0.335876464843750,-0.263946533203125,...
-% 	-0.267242431640625,-0.255554199218750,-0.131347656250000];
-%x=[1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-%x=X([1:1000000],1);
+
 x=X([1:length(X)],1);
 %x=X([7500+128:7700],1);
-%%%%  µÚÒ»¸öÉùµÀ
+%%%%  ç¬¬ä¸€ä¸ªå£°é“
 %%%%  8th lowpass filter
-m1=1557+1;    %»ØÒô¼ä¸ô0.0353
+m1=1557+1;    %å›éŸ³é—´éš”0.0353
 num1 = zeros(m1,1);
 num1(m1-1)=1;
-num1(m1) = -0.2;   %Ô½´ó»ØÒôÔ½³¤
+num1(m1) = -0.2;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 
 den1 = zeros(m1,1);
 den1(1)=1;
 den1(2)=-0.2;
-den1(m1) = -f;   %Ô½´ó»ØÒôÔ½³¤
+den1(m1) = -f;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 lowpass1=filter([1,den1(2)],den1,x/n);
 
-m2=1617+1;    %»ØÒô¼ä¸ô0.0367
+m2=1617+1;    %å›éŸ³é—´éš”0.0367
 num2 = zeros(m2,1);
 num2(m2-1)=1;
-num2(m2) = -0.2;   %Ô½´ó»ØÒôÔ½³¤
+num2(m2) = -0.2;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 
 den2 = zeros(m2,1);
 den2(1)=1;
 den2(2)=-0.2;
-den2(m2) = -f;   %Ô½´ó»ØÒôÔ½³¤
+den2(m2) = -f;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 lowpass2=filter([1,den2(2)],den2,x/n);
 
-m3=1491+1;    %»ØÒô¼ä¸ô0.0338
+m3=1491+1;    %å›éŸ³é—´éš”0.0338
 num3 = zeros(m3,1);
 num3(m3-1)=1;
-num3(m3) = -0.2;   %Ô½´ó»ØÒôÔ½³¤
+num3(m3) = -0.2;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 
 den3 = zeros(m3,1);
 den3(1)=1;
 den3(2)=-0.2;
-den3(m3) = -f;   %Ô½´ó»ØÒôÔ½³¤
+den3(m3) = -f;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 lowpass3=filter([1,den3(2)],den3,x/n);
 
-m4=1422+1;    %»ØÒô¼ä¸ô0.0322
+m4=1422+1;    %å›éŸ³é—´éš”0.0322
 num4 = zeros(m4,1);
 num4(m4-1)=1;
-num4(m4) = -0.2;   %Ô½´ó»ØÒôÔ½³¤
+num4(m4) = -0.2;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 
 den4 = zeros(m4,1);
 den4(1)=1;
 den4(2)=-0.2;
-den4(m4) = -f;   %Ô½´ó»ØÒôÔ½³¤
+den4(m4) = -f;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 lowpass4=filter([1,den4(2)],den4,x/n);
 
 low1=lowpass1+lowpass2+lowpass3+lowpass4;
 
-m5=1277+1;    %»ØÒô¼ä¸ô0.02895
+m5=1277+1;    %å›éŸ³é—´éš”0.02895
 num5 = zeros(m5,1);
 num5(m5)=1;
-num5(m5) = -0.2;   %Ô½´ó»ØÒôÔ½³¤
+num5(m5) = -0.2;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 
 den5 = zeros(m5,1);
 den5(1)=1;
 den5(2)=-0.2;
-den5(m5) = -f;   %Ô½´ó»ØÒôÔ½³¤
+den5(m5) = -f;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 lowpass5=filter([1,den5(2)],den5,x/n);
 
-m6=1356+1;    %»ØÒô¼ä¸ô0.0307
+m6=1356+1;    %å›éŸ³é—´éš”0.0307
 num6 = zeros(m6,1);
 num6(m6)=1;
-num6(m6) = -0.2;   %Ô½´ó»ØÒôÔ½³¤
+num6(m6) = -0.2;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 
 den6 = zeros(m6,1);
 den6(1)=1;
 den6(2)=-0.2;
-den6(m6) = -f;   %Ô½´ó»ØÒôÔ½³¤
+den6(m6) = -f;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 lowpass6=filter([1,den6(2)],den6,x/n);
 
-m7=1188+1;    %»ØÒô¼ä¸ô0.0269
+m7=1188+1;    %å›éŸ³é—´éš”0.0269
 num7 = zeros(m7,1);
 num7(m7-1)=1;
-num7(m7) = -0.2;   %Ô½´ó»ØÒôÔ½³¤
+num7(m7) = -0.2;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 
 den7 = zeros(m7,1);
 den7(1)=1;
 den7(2)=-0.2;
-den7(m7) = -f;   %Ô½´ó»ØÒôÔ½³¤
+den7(m7) = -f;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 lowpass7=filter([1,den7(2)],den7,x/n);
 
-m8=1116+1;    %»ØÒô¼ä¸ô0.0253
+m8=1116+1;    %å›éŸ³é—´éš”0.0253
 num8 = zeros(m8,1);
 num8(m8-1)=1;
-num8(m8) = -0.2;   %Ô½´ó»ØÒôÔ½³¤
+num8(m8) = -0.2;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 
 den8 = zeros(m8,1);
 den8(1)=1;
 den8(2)=-0.2;
-den8(m8) = -f;   %Ô½´ó»ØÒôÔ½³¤
+den8(m8) = -f;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 lowpass8=filter([1,den8(2)],den8,x/n);
 
 low2=lowpass5+lowpass6+lowpass7+lowpass8;
@@ -151,116 +134,102 @@ allpass2=filter(numallpass2,denallpass2,allpass1);
 %sound(allpass2,FS);
 %audiowrite( 'G:\Workfiles\Reverberation\experiment\Algorithm Reverberation\Schroeder Reverberation\yuyin\yuyin_reverb_test.wav',allpass2,FS);
 %audiowrite( 'G:\Workfiles\Reverberation\experiment\myreverb\guitar_sound\guitar_reverb_test.wav',allpass,FS);
-%audiowrite( 'G:\Workfiles\Reverberation\experiment\Algorithm Reverberation\ÅÄÊÖÉù\clap_reverb_Schroeder_test.wav',allpass2,FS);
+%audiowrite( 'G:\Workfiles\Reverberation\experiment\Algorithm Reverberation\æ‹æ‰‹å£°\clap_reverb_Schroeder_test.wav',allpass2,FS);
 
 i=23;
-% y = [-0.284149169921875,-0.145568847656250,-0.0744018554687500,0.260559082031250,0.737670898437500,...
-% 	0.948852539062500,0.992553710937500,0.956146240234375,0.679107666015625,0.287017822265625,-0.116699218750000,...
-% 	-0.524963378906250,-0.694641113281250,-0.619689941406250,-0.516754150390625,-0.367980957031250,-0.229705810546875,...
-% 	-0.137939453125000,0.128204345703125,0.465759277343750,0.500183105468750,0.226745605468750,-0.0614624023437500,...
-% 	-0.200866699218750,-0.363708496093750,-0.642333984375000,-0.722656250000000,-0.604919433593750,-0.602478027343750,...
-% 	-0.519592285156250,-0.251556396484375,-0.145385742187500,-0.102142333984375,0.0696105957031250,0.107513427734375,...
-% 	0.0235900878906250,0.00787353515625000,-0.0543823242187500,-0.134552001953125,-0.0986022949218750,-0.0508422851562500,...
-% 	-0.0410766601562500,0.0310058593750000,0.223114013671875,0.445617675781250,0.505432128906250,0.452301025390625,...
-% 	0.482635498046875,0.494934082031250,0.395690917968750,0.333587646484375,0.307800292968750,0.236663818359375,...
-% 	0.159271240234375,0.0686950683593750,-0.00662231445312500,-0.0108642578125000,-0.0312194824218750,-0.0548400878906250,...
-% 	0.00451660156250000,0.0303649902343750,0.0130004882812500,0.0502319335937500,-0.0303344726562500,-0.208923339843750,...
-% 	-0.167419433593750,-0.0798645019531250,-0.234039306640625,-0.335876464843750,-0.263946533203125,...
-% 	-0.267242431640625,-0.255554199218750,-0.131347656250000];
-%y=[1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-%y=X([1:1000000],2);
+
 y=X([1:length(X)],2);
 %y=X([7500+128:7700],2);
-%%%%  µÚ¶ş¸öÉùµÀ
+%%%%  ç¬¬äºŒä¸ªå£°é“
 %%%%  8th lowpass filter
-m12=1557+1+i;    %»ØÒô¼ä¸ô0.0353
+m12=1557+1+i;    %å›éŸ³é—´éš”0.0353
 num12 = zeros(m12,1);
 num12(m12-1)=1;
-num12(m12) = -0.2;   %Ô½´ó»ØÒôÔ½³¤
+num12(m12) = -0.2;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 
 den12 = zeros(m12,1);
 den12(1)=1;
 den12(2)=-0.2;
-den12(m12) = -f;   %Ô½´ó»ØÒôÔ½³¤
+den12(m12) = -f;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 lowpass12=filter([1,den12(2)],den12,y/n);
 
-m22=1617+1+i;    %»ØÒô¼ä¸ô0.0367
+m22=1617+1+i;    %å›éŸ³é—´éš”0.0367
 num22 = zeros(m22,1);
 num22(m22-1)=1;
-num22(m22) = -0.2;   %Ô½´ó»ØÒôÔ½³¤
+num22(m22) = -0.2;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 
 den22 = zeros(m22,1);
 den22(1)=1;
 den22(2)=-0.2;
-den22(m22) = -f;   %Ô½´ó»ØÒôÔ½³¤
+den22(m22) = -f;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 lowpass22=filter([1,den22(2)],den22,y/n);
 
-m32=1491+1+i;    %»ØÒô¼ä¸ô0.0338
+m32=1491+1+i;    %å›éŸ³é—´éš”0.0338
 num32 = zeros(m32,1);
 num32(m32-1)=1;
-num32(m32) = -0.2;   %Ô½´ó»ØÒôÔ½³¤
+num32(m32) = -0.2;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 
 den32 = zeros(m32,1);
 den32(1)=1;
 den32(2)=-0.2;
-den32(m32) = -f;   %Ô½´ó»ØÒôÔ½³¤
+den32(m32) = -f;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 lowpass32=filter([1,den32(2)],den32,y/n);
 
-m42=1422+1+i;    %»ØÒô¼ä¸ô0.0322
+m42=1422+1+i;    %å›éŸ³é—´éš”0.0322
 num42 = zeros(m42,1);
 num42(m42-1)=1;
-num42(m42) = -0.2;   %Ô½´ó»ØÒôÔ½³¤
+num42(m42) = -0.2;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 
 den42 = zeros(m42,1);
 den42(1)=1;
 den42(2)=-0.2;
-den42(m42) = -f;   %Ô½´ó»ØÒôÔ½³¤
+den42(m42) = -f;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 lowpass42=filter([1,den42(2)],den42,y/n);
 
 low12=lowpass12+lowpass22+lowpass32+lowpass42;
 
-m52=1277+1+i;    %»ØÒô¼ä¸ô0.02895
+m52=1277+1+i;    %å›éŸ³é—´éš”0.02895
 num52 = zeros(m52,1);
 num52(m52-1)=1;
-num52(m52) = -0.2;   %Ô½´ó»ØÒôÔ½³¤
+num52(m52) = -0.2;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 
 den52 = zeros(m52,1);
 den52(1)=1;
 den52(2)=-0.2;
-den52(m52) = -f;   %Ô½´ó»ØÒôÔ½³¤
+den52(m52) = -f;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 lowpass52=filter([1,den52(2)],den52,y/n);
 
-m62=1356+1+i;    %»ØÒô¼ä¸ô0.0307
+m62=1356+1+i;    %å›éŸ³é—´éš”0.0307
 num62 = zeros(m62,1);
 num62(m62-1)=1;
-num62(m62) = -0.2;   %Ô½´ó»ØÒôÔ½³¤
+num62(m62) = -0.2;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 
 den62 = zeros(m62,1);
 den62(1)=1;
 den62(2)=-0.2;
-den62(m62) = -f;   %Ô½´ó»ØÒôÔ½³¤
+den62(m62) = -f;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 lowpass62=filter([1,den62(2)],den62,y/n);
 
-m72=1188+1+i;    %»ØÒô¼ä¸ô0.0269
+m72=1188+1+i;    %å›éŸ³é—´éš”0.0269
 num72 = zeros(m72,1);
 num72(m72-1)=1;
-num72(m72) = -0.2;   %Ô½´ó»ØÒôÔ½³¤
+num72(m72) = -0.2;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 
 den72 = zeros(m72,1);
 den72(1)=1;
 den72(2)=-0.2;
-den72(m72) = -f;   %Ô½´ó»ØÒôÔ½³¤
+den72(m72) = -f;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 lowpass72=filter([1,den72(2)],den72,y/n);
 
-m82=1116+1+i;    %»ØÒô¼ä¸ô0.0253
+m82=1116+1+i;    %å›éŸ³é—´éš”0.0253
 num82 = zeros(m82,1);
 num82(m82-1)=1;
-num82(m82) = -0.2;   %Ô½´ó»ØÒôÔ½³¤
+num82(m82) = -0.2;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 
 den82 = zeros(m82,1);
 den82(1)=1;
 den82(2)=-0.2;
-den82(m82) = -f;   %Ô½´ó»ØÒôÔ½³¤
+den82(m82) = -f;   %è¶Šå¤§å›éŸ³è¶Šé•¿
 lowpass82=filter([1,den82(2)],den82,y/n);
 
 low22=lowpass52+lowpass62+lowpass72+lowpass82;
@@ -288,36 +257,7 @@ denallpass22(1)=1;
 denallpass22(n22)=-g22;
 allpass22=filter(numallpass22,denallpass22,allpass12);
 
-% n3=441;  %0.01
-% g3=0.5;
-% numallpass3=zeros(n3,1);
-% numallpass3(1)=-g3;
-% numallpass3(3)=1;
-% 
-% denallpass3=zeros(n3,1);
-% denallpass3(1)=1;
-% denallpass3(n3)=-g3;
-% allpass3=filter(numallpass3,denallpass3,allpass2);
-% 
-% n4=341;  %0.0077
-% g4=0.5;
-% numallpass4=zeros(n4,1);
-% numallpass4(1)=-g4;
-% numallpass4(3)=1;
-% 
-% denallpass4=zeros(n4,1);
-% denallpass4(1)=1;
-% denallpass4(n4)=-g4;
-% allpass4=filter(numallpass4,denallpass4,allpass3);
-
 allpass=[allpass2,allpass22];
 
 %sound(allpass,FS);
-%audiowrite( 'G:\Workfiles\Reverberation\experiment\Algorithm Reverberation\Schroeder Reverberation\yuyin\yuyin_reverb_test.wav',allpass2,FS);
-%audiowrite( 'G:\Workfiles\Reverberation\experiment\myreverb\guitar_sound\guitar_reverb_test.wav',allpass,FS);
-%audiowrite( 'G:\Workfiles\Reverberation\experiment\Algorithm Reverberation\music\nihaiyaowo_reverb0.94.wav',allpass,FS);
-%audiowrite( 'G:\Workfiles\Reverberation\experiment\Algorithm Reverberation\music\donganzhenming_reverb0.75.wav',allpass,FS);
-%audiowrite( 'G:\Workfiles\Reverberation\experiment\Algorithm Reverberation\music\yanyuan_reverb0.74.wav',allpass,FS);
-%audiowrite( 'G:\Workfiles\Reverberation\experiment\Algorithm Reverberation\ÅÄÊÖÉù\clap_reverb_Schroeder_test.wav',allpass,FS);
-%audiowrite( 'F:\JOYO\¸Ä½øSchroeder Reverberation£¨Ğ§¹û½ÏºÃ£©\²âÊÔÊı¾İ\clap0.94.mp3',allpass,FS);
-audiowrite('F:\JOYO\¸Ä½øSchroeder Reverberation£¨Ğ§¹û½ÏºÃ£©\²âÊÔÊı¾İ\music\See You Again0.84.wav',allpass,FS);
+audiowrite('F:\æ”¹è¿›Schroeder Reverberationï¼ˆæ•ˆæœè¾ƒå¥½ï¼‰\æµ‹è¯•æ•°æ®\music\See You Again0.84.wav',allpass,FS);
